4db578d73d8ce60620bfff1629ac2846fa4a1307
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-20 10:06:13 -0800

    Added Spring initializer archive.

---
+++ ch01/carddatabase/src/test/java/com/packt/carddatabase/CarddatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.carddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarddatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
