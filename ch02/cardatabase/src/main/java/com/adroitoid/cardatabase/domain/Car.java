f942d23d05bee5a0c9d40b44166a55e7513b2776
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-20 20:14:27 -0800

    Syncing the Car Entity and MariaDB.

--- ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/domain/Car.java
+++ ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/domain/Car.java
@@ -6,10 +6,85 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Car {
+public class Car
+{
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
   private long id;
   private String brand, model, color, registerNumber;
   private int year, price;
+
+  public Car() {}
+
+  public Car(String brand, String model, String color, 
+    String registerNumber, int year, int price)
+  {
+    super();
+    this.brand = brand;
+    this.model = model;
+    this.color = color;
+    this.registerNumber = registerNumber;
+    this.year = year;
+    this.price = price;
+  }
+  
+  public String getBrand()
+  {
+	return brand;
+  }
+  
+  public void setBrand(String brand)
+  {
+    this.brand = brand;
+  }
+  
+  public String getModel()
+  {
+    return model;
+  }
+  
+  public void setModel(String model)
+  {
+    this.model = model;
+  }
+  
+  public String getColor()
+  {
+    return color;
+  }
+  
+  public void setColor(String color)
+  {
+    this.color = color;
+  }
+  
+  public String getRegisterNumber()
+  {
+    return registerNumber;
+  }
+  
+  public void setRegisterNumber(String registerNumber)
+  {
+    this.registerNumber = registerNumber;
+  }
+  
+  public int getYear()
+  {
+    return year;
+  }
+  
+  public void setYear(int year)
+  {
+    this.year = year;
+  }
+  
+  public int getPrice()
+  {
+    return price;
+  }
+  
+  public void setPrice(int price)
+  {
+    this.price = price;
+  }
 }
