521f1704aa031d8c8d0d91fb405981f750e9523b
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-21 14:45:25 -0800

    Inserting a few cars to the database.
    SELECT * FROM CAR

--- ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
+++ ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
@@ -1,15 +1,21 @@
 package com.adroitoid.cardatabase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import com.adroitoid.cardatabase.domain.Car;
+import com.adroitoid.cardatabase.domain.CarRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication
 {
+  @Autowired 
+  private CarRepository repository;
+  
   private static final Logger logger = 
 	LoggerFactory.getLogger(CardatabaseApplication.class);
   
@@ -23,7 +29,13 @@ public class CardatabaseApplication
   CommandLineRunner runner()
   {
     return args -> {
-      // Place your code here
+    	// Save demo data to database
+        repository.save(new Car("Ford", "Mustang", "Red",
+         "ADF-1121", 2017, 59000));
+        repository.save(new Car("Nissan", "Leaf", "White",
+         "SSJ-3002", 2014, 29000));
+        repository.save(new Car("Toyota", "Prius", "Silver",
+         "KKO-0212", 2018, 39000));
     };
   }
   
