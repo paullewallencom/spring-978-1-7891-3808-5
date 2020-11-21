cdea124235669514625a2d02e1c94a29e2c8a403
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-21 14:29:48 -0800

    Creating CRUD repositories
    localhost:8080/h2-console
    
    JDBC URL Field: Look in log for url
    Password Field: empty

--- ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
+++ ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
@@ -1,18 +1,30 @@
 package com.adroitoid.cardatabase;
 
+import org.springframework.boot.CommandLineRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication
 {
   private static final Logger logger = 
 	LoggerFactory.getLogger(CardatabaseApplication.class);
+  
   public static void main(String[] args)
   {
     SpringApplication.run(CardatabaseApplication.class, args);
     logger.info("Hello Spring Boot");
   }
+  
+  @Bean
+  CommandLineRunner runner()
+  {
+    return args -> {
+      // Place your code here
+    };
+  }
+  
 }
---
+++ ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/domain/CarRepository.java
@@ -0,0 +1,8 @@
+package com.adroitoid.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository <Car, Long>
+{
+
+}
