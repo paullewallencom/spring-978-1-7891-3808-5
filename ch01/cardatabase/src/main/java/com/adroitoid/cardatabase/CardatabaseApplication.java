56d583711afa5aee1c7da28b689a9c6bdee2ca49
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-20 18:14:01 -0800

    Added logging capability.

--- ch01/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
+++ ch01/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,18 @@
 package com.adroitoid.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardatabaseApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CardatabaseApplication.class, args);
-	}
-
+public class CardatabaseApplication
+{
+  private static final Logger logger = 
+	LoggerFactory.getLogger(CardatabaseApplication.class);
+  public static void main(String[] args)
+  {
+    SpringApplication.run(CardatabaseApplication.class, args);
+    logger.info("Hello Spring Boot");
+  }
 }
