c9f83e76a7646bfa2ad5eba92bc397921e968086
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-20 19:59:17 -0800

    Adding fields to Car Entity.

--- ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/domain/Car.java
+++ ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/domain/Car.java
@@ -1,8 +1,15 @@
 package com.adroitoid.cardatabase.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Car {
-
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private long id;
+  private String brand, model, color, registerNumber;
+  private int year, price;
 }
