beea8488f6172e61b55cbaccef007e25ed8410eb
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-27 17:27:21 -0800

    Adding owner names to test data.

--- ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
+++ ch02/cardatabase/src/main/java/com/adroitoid/cardatabase/CardatabaseApplication.java
@@ -9,12 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import com.adroitoid.cardatabase.domain.Car;
 import com.adroitoid.cardatabase.domain.CarRepository;
+import com.adroitoid.cardatabase.domain.Owner;
+import com.adroitoid.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication
 {
   @Autowired 
   private CarRepository repository;
+  
+  @Autowired 
+  private OwnerRepository orepository;
   
   private static final Logger logger = 
 	LoggerFactory.getLogger(CardatabaseApplication.class);
@@ -29,13 +34,22 @@ public class CardatabaseApplication
   CommandLineRunner runner()
   {
     return args -> {
-    	// Save demo data to database
-        repository.save(new Car("Ford", "Mustang", "Red",
-         "ADF-1121", 2017, 59000));
-        repository.save(new Car("Nissan", "Leaf", "White",
-         "SSJ-3002", 2014, 29000));
-        repository.save(new Car("Toyota", "Prius", "Silver",
-         "KKO-0212", 2018, 39000));
+    	// Add owner objects and save these to db
+        Owner owner1 = new Owner("John" , "Johnson");
+        Owner owner2 = new Owner("Mary" , "Robinson");
+        orepository.save(owner1);
+        orepository.save(owner2);
+
+        // Add car object with link to owners and save these to db.
+        Car car = new Car("Ford", "Mustang", "Red", 
+            "ADF-1121", 2017, 59000, owner1);
+        repository.save(car);
+        car = new Car("Nissan", "Leaf", "White",
+            "SSJ-3002", 2014, 29000, owner2);
+        repository.save(car);
+        car = new Car("Toyota", "Prius", "Silver",
+            "KKO-0212", 2018, 39000, owner2);
+        repository.save(car);
     };
   }
   
