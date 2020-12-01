f9783966a6e823ce5f2bc8e8de699342f080b616
Paul Lewallen <paul.lewallen@gmail.com>
2020-12-01 13:40:23 -0800

    Cars view exposed at: localhost:8080/cars

--- ch03/cardatabase/src/main/java/com/adroitoid/cardatabase/web/CarController.java
+++ ch03/cardatabase/src/main/java/com/adroitoid/cardatabase/web/CarController.java
@@ -1,13 +1,20 @@
 package com.adroitoid.cardatabase.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.adroitoid.cardatabase.domain.Car;
+import com.adroitoid.cardatabase.domain.CarRepository;
 
 @RestController
 public class CarController
 {
+    @Autowired
+    private CarRepository repository;
+
     @RequestMapping("/cars")
-    public Iterable<Car> getCars()
-    {
-        ///
+    public Iterable<Car> getCars() {
+        return repository.findAll();
     }
 }
